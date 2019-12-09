2ee976215ee48bf02af0c97f8962326be8068a7f
Srujan Aditya <srujan.aditya@thomsonreuters.com>
2019-12-09 14:04:47 +0530

    columns parsers

--- Homework1/task2.js
+++ Homework1/task2.js
@@ -1,6 +1,14 @@
 const csvFilePath = './data.csv';
 const csv = require('csvtojson');
-csv()
+csv({
+	colParser:{
+		"column1":"string",
+        "column2":"string",
+        "column3":"number",
+        "column4":"number"
+	},
+	checkType:true
+})
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
 	console.log(jsonObj);
