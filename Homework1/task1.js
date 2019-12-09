233e5af4f00d2aef6051482812e4efbc18428fb0
Srujan Aditya <srujan.aditya@thomsonreuters.com>
2019-12-09 14:06:54 +0530

    updated

--- Homework1/task1.js
+++ Homework1/task1.js
@@ -3,7 +3,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   let chunk;
   while ((chunk = process.stdin.read()) !== null) {
-    chunk = chunk.split('').reverse().join('') + '\n';
-    process.stdout.write(`${chunk}`);
+    process.stdout.write(`${chunk.split('').reverse().join('')}\n`);
   }
 });
