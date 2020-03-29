5de0fa5d7872cd7c8b66be83875d057ea31fe4ad
srujanaditya@gmail.com <srujanaditya@gmail.com>
2020-03-30 01:00:36 +0530

    updated

--- Homework3/test/user-controller.test.js
+++ Homework3/test/user-controller.test.js
@@ -5,20 +5,20 @@ const { userModal } = require('./../modals/users/user-modal');
 const userService = require('./../services/users/user-service');
 
 const users = [
-    {
-        "id": "001",
-        "login": "admin@gmail.com",
-        "password": "$2b$10$fVREBieaUKxmEXaK.mGO3eyHVoqcVBtmprIkKklEAqgbKbzbDAKga",
-        "age": 22,
-        "isDeleted": false
-    },
-    {
-        "id": "002",
-        "login": "samp@gmail.com",
-        "password": "$2b$10$fVREBieaUKxmEXaK.mGO3eyHVoqcVBtmprIkKklEAqgbKbzbDAKga",
-        "age": 23,
-        "isDeleted": false
-    }
+	{
+		"id": "001",
+		"login": "admin@gmail.com",
+		"password": "$2b$10$fVREBieaUKxmEXaK.mGO3eyHVoqcVBtmprIkKklEAqgbKbzbDAKga",
+		"age": 22,
+		"isDeleted": false
+	},
+	{
+		"id": "002",
+		"login": "samp@gmail.com",
+		"password": "$2b$10$fVREBieaUKxmEXaK.mGO3eyHVoqcVBtmprIkKklEAqgbKbzbDAKga",
+		"age": 23,
+		"isDeleted": false
+	}
 ];
 describe('USER Controller testing Endpoints', () => {
 
@@ -73,12 +73,76 @@ describe('USER Controller testing Endpoints', () => {
 		expect(res.statusCode).toEqual(404);
 		done();
 	});
-	// it('should insert new User', async (done) => {
-	// 	userService.updateUserData = jest.fn();
-	// 	userService.updateUserData.mockReturnValue([users[1]]);
-	// 	const res = await request.put('/api/users/:003').set('x-access-token', `${token}`);
-	// 	console.log(res);
-	// 	expect(res.statusCode).toEqual(404);
-	// 	done();
-	// });
+	it('should update user details', async (done) => {
+		userService.updateUserData = jest.fn();
+		userService.updateUserData.mockReturnValue([users[1]]);
+		const res = await request.put('/api/users/:003')
+			.set('x-access-token', `${token}`)
+			.send({
+				"id": "006",
+				"login": "admin4@gmail.com",
+				"password": "password",
+				"age": 22
+			});
+		expect(res.statusCode).toEqual(200);
+		done();
+	});
+	it('should update user details but an id already already exist', async (done) => {
+		userService.updateUserData = jest.fn();
+		userService.updateUserData.mockReturnValue([]);
+		const res = await request.put('/api/users/:003')
+			.set('x-access-token', `${token}`)
+			.send({
+				"id": "006",
+				"login": "admin4@gmail.com",
+				"password": "password",
+				"age": 22
+			});
+		expect(res.statusCode).toEqual(404);
+		done();
+	});
+	it('should add new user', async (done) => {
+		userService.addUser = jest.fn();
+		userService.addUser.mockReturnValue();
+		const res = await request.post('/api/addUser')
+			.set('x-access-token', `${token}`)
+			.send({
+				"id": "006",
+				"login": "admin4@gmail.com",
+				"password": "password",
+				"age": 22
+			});
+		expect(res.statusCode).toEqual(200);
+		done();
+	});
+	it('should add new user', async (done) => {
+		userService.addUser = jest.fn();
+		userService.addUser.mockReturnValue();
+		const res = await request.post('/api/addUser')
+			.set('x-access-token', `${token}`)
+			.send({
+				"id": "006",
+				"login": "admin4@gmail.com",
+				"password": "password",
+				"age": 22
+			});
+		expect(res.statusCode).toEqual(200);
+		done();
+	});
+	it('should add new user, throws an error', async (done) => {
+		userService.addUser = jest.fn();
+		userService.addUser.mockImplementation(() => {
+			throw new Error();
+		});
+		const res = await request.post('/api/addUser')
+			.set('x-access-token', `${token}`)
+			.send({
+				"id": "006",
+				"login": "admin4@gmail.com",
+				"password": "password",
+				"age": 22
+			});
+		expect(res.statusCode).toEqual(422);
+		done();
+	});
 });
